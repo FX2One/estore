29b38804e3c5b16496150165e94583030f94d362
fx2one <dfoxik@gmail.com>
2021-01-28 20:58:27 +0100

    decorators

--- store/models.py
+++ store/models.py
@@ -45,12 +45,29 @@ class Order(models.Model):
     def __str__(self):
         return str(self.id)
 
+    @property
+    def get_cart_total(self):
+        orderitems = self.OrderItem_set.all()
+        total = sum([item.get_total for item in orderitems])  #calculate the sum of item.get_total value, to calculate all items
+        return total
+
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems]) #total is going to be set to quantity, to know how many items are in the cart
+        return total
+
 #orderItem with Many to One Relationship, Order Item within our Cart which have multiple items in it
 class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL,null=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
     quantity = models.IntegerField(default=0, null=True,blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
 
 #customer information
 class ShippingAddress(models.Model):
