c3adb5baef7e6c7afc910f054bf8e14f8fffb771
FX2One <dfoxik@gmail.com>
2021-01-28 01:15:16 +0100

    cart views and html update

--- store/models.py
+++ store/models.py
@@ -14,7 +14,6 @@ class Customer(models.Model):
     name = models.CharField(max_length=150, null=True)
     email = models.CharField(max_length=200, null=True)
 
-
     def __str__(self):
         return self.name
 
